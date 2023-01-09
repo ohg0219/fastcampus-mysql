2b3fb165ead2797b347bceab681fd8020467eff0
Hayden <ohg0219@mibank.me>
2023-01-09 10:51:52 +0900

    회원이름 변경 및 단위 테스트

---
+++ src/test/java/com/example/fastcampusmysql/domain/member/MemberTest.java
@@ -0,0 +1,43 @@
+package com.example.fastcampusmysql.domain.member;
+
+import com.example.fastcampusmysql.util.MemberFixtureFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MemberTest {
+
+    @Test
+    @DisplayName("회원은 닉네임을 변경할 수 있다.")
+    public void testChangeNickname() {
+        var member = MemberFixtureFactory.create();
+
+        var expected = "pnu";
+
+        member.changeNickname(expected);
+
+        Assertions.assertEquals(expected, member.getNickname());
+
+//        LongStream.range(0, 10)
+//                .mapToObj(MemberFixtureFactory::create)
+//                .forEach(m -> {
+//                    System.out.println(m.getNickname());
+//                });
+    }
+
+
+    @Test
+    @DisplayName("회원의 닉네임을 10자를 초과할 수 없다")
+    public void testNicknameMaxLength() {
+        var member = MemberFixtureFactory.create();
+
+        var overMaxLengthName = "pnupnupnupnu";
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            member.changeNickname(overMaxLengthName);
+        });
+
+    }
+
+
+}
